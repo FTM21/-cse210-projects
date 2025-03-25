2cfb201357d46bf4e6eb866f5869eb83e0acd748
FTM21 <f.tungumuna@gmail.com>
2025-03-25 13:18:47 +0100

    alteracoes2

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -173,3 +173,8 @@ namespace ScriptureMemory
         }
     }
 }
+
+//Comments: - Exceeding requirements: The program effectively utilizes encapsulation with private member variables in the Scripture, 
+//Word, and Reference classes. It features clear responsibilities for each class, displaying scriptures accurately
+// and hiding words interactively. Additionally, enhancements include a robust scripture library more than required, 
+//random selection of this scripture, and a user-friendly interface, exceeding basic requirements.
