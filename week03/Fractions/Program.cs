c816258b8f202a31615ed4af60345a98a2a49318
FTM21 <f.tungumuna@gmail.com>
2025-03-25 13:17:51 +0100

    alteracoes2

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
     }
 }
+
+//Comments: - Exceeding requirements: The program effectively utilizes encapsulation with private member variables in the Scripture, 
+//Word, and Reference classes. It features clear responsibilities for each class, displaying scriptures accurately
+// and hiding words interactively. Additionally, enhancements include a robust scripture library more than required, 
+//random selection of this scripture, and a user-friendly interface, exceeding basic requirements.
